26dce10bc4b843d58592e9edea9cfab8c9991747
MATAYOSHI RYO <yuushi20122@gmail.com>
2026-08-18 16:01:36 +0900

    20260818

--- 20260818/20260818_main.cpp
+++ 20260818/20260818_main.cpp
@@ -8,22 +8,23 @@ int main(void)
 {
 	//変数宣言＆初期化
 	int hp{ 100 };
+	int* php = { &hp };
 
 	//初期値の表示
-	cout << format("PlayerHP : {}", hp) << endl;
+	cout << format("PlayerHP : {}", *php) << endl;
 	cout << "\n";
 
 	//Damage関数＆表示
 	Damage(&hp);
 	cout << format("{}ダメージを受けた....",HIT_DAMAGE) << endl;
-	cout << format("PlayerHP : {}", hp) << endl;
+	cout << format("PlayerHP : {}", *php) << endl;
 
 	cout << "\n";
 
 	//Heal関数＆表示
 	Heal(&hp);
 	cout << format("{}回復した！", HEAL_POINT) << endl;
-	cout << format("PlayerHP : {}", hp) << endl;
+	cout << format("PlayerHP : {}", *php) << endl;
 
 	return 0;
 }
